5091cdc5da6abe7e48654e978c00fefe1b86cbc9
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-07-01 22:11:20 -0500

    Add some aliases, make orders work

--- client.rb
+++ client.rb
@@ -1,3 +1,7 @@
+#   require './client'; Client.pnd_time_based(0.001, Client.currency_price("BTC-ANS"), 10, 'BTC-ANS')
+
+#   require './client'; Client.pnd(0.001, 0.98, 1.02, 'BTC-XEL', true)
+
 require 'json'
 require 'base64'
 require 'uri'
@@ -7,7 +11,6 @@ require 'time'
 
 HOST="https://bittrex.com/api/v1.1"
 
-#   z = Client.pnd(0.01, 0.998, 1.01, 'ANS')
 
 class Client
   class << self
@@ -45,14 +48,17 @@ class Client
     def get_orders
       get("market/getopenorders")
     end
+    alias_method :orders, :get_orders
 
     def get_balances
       get 'account/getbalances'
     end
+    alias_method :balances, :get_balances
 
     def get_balance(currency = 'BTC')
       get 'account/getbalance', {currency: currency}
     end
+    alias_method :balance, :get_balance
 
     def get_order(order_id)
       get 'account/getorder', {uuid: order_id}
@@ -82,57 +88,57 @@ class Client
 
       currencies_to_buy = (amount / buy_price).round(8)
       # make buy order
-      log "Putting buy order for #{fts currencies_to_buy} #{market} at #{fts buy_price} BTC, took #{Time.now - start_time} ms"
+      log "Putting buy order for #{fts currencies_to_buy} #{market} at #{fts buy_price} BTC, took #{fts Time.now - start_time} ms"
       order = market_buy_limit(market, currencies_to_buy, buy_price)
       log "Order id is #{order["uuid"]}"
 
       # wait
+      log "Sleeping #{wait_seconds} seconds..."
       sleep wait_seconds # seconds
 
       # cancel order or make sell order
       if get_order(order["uuid"])["Closed"].nil?
         # cancel order
         cancel_order(order["uuid"])
-        log "Could not buy, canceling order"
+        log "Could not buy, order canceled"
       else
         # make sell order, take profit
-        current_price = currency_price(market)
-        log "Putting sell order for #{fts currencies_to_buy} #{currency} at #{fts current_price} BTC"
-        order = market_sell_limit(market, currencies_to_buy, current_price)
+        sell_price = currency_price(market)
+        log "Putting sell order for #{fts currencies_to_buy} #{market} at #{fts sell_price} BTC"
+        order = market_sell_limit(market, currencies_to_buy, sell_price)
         log "Order id is #{order["uuid"]}"
 
         # wait
         while (res = get_order(order["uuid"]))["Closed"].nil?
-          price = currency_price(currency_to_buy)
-          log "[#{buy_count}] Waiting for sell order to complete... current_price is #{fts price.round(8)}, sold set at #{fts sell_at_price}"
+          price = currency_price(market)
+          log "[#{buy_count}] Waiting for sell order to complete... current_price is #{fts price.round(8)}, sold set at #{fts sell_price}"
           sleep 0.5
         end
         cost = currencies_to_buy * buy_price * 1.0025 # 0.0025% fee
-        reward = currencies_to_buy * current_price * 0.9975 # 0.0025% fee
+        reward = currencies_to_buy * sell_price * 0.9975 # 0.0025% fee
         profit = reward - cost
         log "SOLD! profit = #{fts(profit)}"
       end
     end
 
-    def pnd(amount, buy_when, sell_when, currency, recursive = false)
+    def pnd(amount, buy_when, sell_when, market, recursive = false)
       @buy_count ||= 0
       @buy_count += 1
       start_time = Time.now
-      amount = 0.01 # BTC
-      currency_to_buy = "BTC-#{currency}" # currency
+      amount = 0.001 # BTC
 
-      current_price = currency_price(currency_to_buy)
+      current_price = currency_price(market)
       buy_at_price = (current_price * buy_when).round(8)
       currencies_to_buy = (amount / buy_at_price).round(8)
 
       # make buy order
       log "Current price is #{fts current_price}"
-      log "Putting buy order for #{fts currencies_to_buy} #{currency} at #{fts buy_at_price} BTC, took #{Time.now - start_time} ms"
-      order = market_buy_limit(currency_to_buy, currencies_to_buy, buy_at_price)
+      log "Putting buy order for #{fts currencies_to_buy} #{market} at #{fts buy_at_price} BTC, took #{fts Time.now - start_time} ms"
+      order = market_buy_limit(market, currencies_to_buy, buy_at_price)
       log "Order id is #{order["uuid"]}"
       # wait
       while (res = get_order(order["uuid"]))["Closed"].nil?
-        price = currency_price(currency_to_buy)
+        price = currency_price(market)
         log "[#{buy_count}] Waiting for buy order to complete... current_price is #{fts price.round(8)}, bought set at #{fts buy_at_price}"
         sleep 0.5
       end
@@ -141,13 +147,13 @@ class Client
 
       # make sell order
       sell_at_price = (current_price * sell_when).round(8)
-      log "Putting sell order for #{fts currencies_to_buy} #{currency} at #{fts sell_at_price} BTC"
-      order = market_sell_limit(currency, currencies_to_buy, sell_at_price)
+      log "Putting sell order for #{fts currencies_to_buy} #{market} at #{fts sell_at_price} BTC"
+      order = market_sell_limit(market, currencies_to_buy, sell_at_price)
       log "Order id is #{order["uuid"]}"
 
       # wait
       while (res = get_order(order["uuid"]))["Closed"].nil?
-        price = currency_price(currency_to_buy)
+        price = currency_price(market)
         log "[#{buy_count}] Waiting for sell order to complete... current_price is #{fts price.round(8)}, sold set at #{fts sell_at_price}"
         sleep 0.5
       end
@@ -155,7 +161,7 @@ class Client
       reward = currencies_to_buy * sell_at_price * 0.9975 # 0.0025% fee
       profit = reward - cost
       log "SOLD! profit = #{fts(profit)}"
-      pnd(amount, buy_when, sell_when, currency, recursive) if recursive
+      pnd(amount, buy_when, sell_when, market, recursive) if recursive
     end
 
     def fts(n)
