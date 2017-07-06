b429882c2fb73d60e3d3e30420d43245069289d3
Carlos Rodríguez <ecarlos.rodriguez@gmail.com>
2017-07-05 19:39:17 -0500

    Encapsulate bot into class, add bot to logger

--- gany_the_bot.js
+++ gany_the_bot.js
@@ -1,40 +1,38 @@
 // Will analyze the market
 require('dotenv').config();
 var bittrex = require('node.bittrex.api');
-
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.GANY_KEY;
 
-const gany_the_bot = new TelegramBot(token, {polling: true});
+function GanyTheBot() {
+  this.chats = [];
+  // this.chats.push(parseInt(process.env.WARNINGS_GROUP)); // disable for testing
+  this.chats.push(parseInt(process.env.PERSONAL_CHANNEL)); // by default subscribe to my personal account
+  this.token = process.env.GANY_KEY;
 
-this.chats = [];
-// this.chats.push(parseInt(process.env.WARNINGS_GROUP)); // disable for testing
-this.chats.push(parseInt(process.env.PERSONAL_CHANNEL))
+  this.telegram_bot = new TelegramBot(this.token, {polling: true});
+}
 
-gany_the_bot.onText(/\/dudeimking/, (msg, match) => {
-  // Subscribers
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  if (this.chats.includes(msg.chat.id)) {
-    gany_the_bot.sendMessage(chatId, "You are already subscribed");
-  } else {
-    this.chats.push(chatId);
-    gany_the_bot.sendMessage(chatId, "Hello stranger. " + chatId + " subscribed.");
-  }
-});
+GanyTheBot.prototype.start = function() {
+  this.telegram_bot.onText(/\/dudeimking/, (msg, match) => {
+    // Subscribers
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    if (this.chats.includes(msg.chat.id)) {
+      this.telegram_bot.sendMessage(chatId, "You are already subscribed");
+    } else {
+      this.chats.push(chatId);
+      this.telegram_bot.sendMessage(chatId, "Hello stranger. " + chatId + " subscribed.");
+    }
+  });
+}
 
-// gany_the_bot.on('message', (msg) => {
-//   console.log("MESSAGE RECEIVED FROM " + msg.chat.id + ": " + msg);
-//   // TO IMPLEMENT IN FUTURE
-//   // send a message to the chat acknowledging receipt of their message
-//   // gany_the_bot.sendMessage(chatId, 'Received your message');
-// });
-
-function broadcast_message(text) {
+GanyTheBot.prototype.broadcast = function(text) {
   this.chats.forEach(function(chat_id) {
-    gany_the_bot.sendMessage(chat_id, text).catch((error) => {
+    this.telegram_bot.sendMessage(chat_id, text).catch((error) => {
       console.log(error.code);  // => 'ETELEGRAM'
       console.log(error.response.body); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
     });
   });
 }
+
+module.exports = GanyTheBot;
--- logger.js
+++ logger.js
@@ -1,11 +1,15 @@
 var DateTime = require('node-datetime');
 
+const GanyTheBot = require('./gany_the_bot');
+
 function Logger(market) {
   this.market = market;
+  this.gany_the_bot = new GanyTheBot();
 }
 
 Logger.prototype.log = function(str) {
   var time = DateTime.create()._now;
+  this.gany_the_bot.broadcast(str);
   console.log("[" + time + "] [" + this.market + "] " + str);
 }
 
