35fca73e5d2dc860e0176cbd839e623029ba17f4
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-07-10 22:14:47 -0500

    Start tracking tickers from APIs through events in detektor

--- detektor.js
+++ detektor.js
@@ -5,96 +5,88 @@ const PumpHandler = require('./pump_handler.js');
 require('./protofunctions.js');
 
 // REAL CODING
-function Detektor(logger, api, markets, pump_events, test_mode) {
-  this.logger = logger;
-  this.api = api;
-  this.markets = markets;
-  this.pumps_bought = [];
-  this.pump_events = pump_events;
-  this.pumps = [];
-  this.count = 0;
-  this.test_mode = test_mode;
+function Detektor(logger, pump_events, test_mode) {
+  this.logger = logger
+  this.pump_events = pump_events
+  this.test_mode = test_mode
+  this.market_data = {}
+
+  this.market_data
+  this.tickers = {}
+  this.tickers_history = {}
+
+  this.pump_events.on('marketupdate', (operation, exchange, market, data) => {
+    if (operation == 'TICKER') {
+      this.tickers[exchange] = this.tickers[exchange] || {}
+      this.tickers[exchange][market] = data;
+    }
+    // console.log(operation + " | Exchange: " + exchange + " | Market: " + market)
+  })
+  this.track_tickers_history()
 }
 
-Detektor.prototype.start = function() {
-  // Configure listeners
-  // this.logger.log('Detektor', 'Starting watch');
-  setTimeout(() => {
-    var self = this;
-    self.api.websockets.subscribe(self.markets, function(data) {
-      if (data.M === 'updateExchangeState') {
-        data.A.forEach(function(data_for) {
-          self.logger.log_to_file(JSON.stringify(data_for));
-          self.analyze_market(data_for);
-        });
-      }
-    });
-  }, 1000);
-  // // Listen to markets updates
-  // setTimeout(function() {
-  //     this.api.websockets.listen( function( data ) {
-  //     if (data.M === 'updateSummaryState') {
-  //       console.log("SECOND UPDATE", data)
-  //       data.A.forEach(function(data_for) {
-  //         data_for.Deltas.forEach(function(marketsDelta) {
-  //           // console.log('Ticker Update for '+ marketsDelta.MarketName, marketsDelta);
-  //         });
-  //       });
-  //     }
-  //   });
-  // }, 5000); // give time for first subscriber to subscribe
-
-}
-
-Detektor.prototype.analyze_market = function(data) {
-  result = false;
-  buy_at = 0;
-  sell_at = 0;
-  btc_amount = 0;
-  market_name = data.MarketName;
-  self = this;
-  if (data.Fills.length > 0) {
-    buys = data.Fills.filter(function(e) { return e.OrderType == 'BUY'; });
-    buy_amount = buys.length == 0 ? 0 : buys.map(function(e) { return e.Quantity * e.Rate; }).sum();
-    first_fill = data.Fills.last();
-    last_fill = data.Fills.first();
-    change = last_fill.Rate / first_fill.Rate;
-    if ( (change > 1.04) && (buys.length > 40 || buy_amount > 3.5) ) {
-      result = true; buy_at = 1.01; sell_at = 1.08; btc_amount = 0.01;
-    }
-    if ( (change > 1.05) && (buys.length > 50 || buy_amount > 5) ) {
-      result = true; buy_at = 1.06; sell_at = 1.5; btc_amount = 0.1;
-    }
-  }
-  if (result) {
-    self.count += 1;
-    if (self.count > 100) {
-      self.logger.log(market_name, "PUMP DETECTED BUT ALREADY REACHED MAX BUY/SELLS")
-    } else {
-      if (self.pumps_bought[market_name] == undefined) {
-        // get ticker info and make BUY order
-        last_fill = data.Fills.first();
-        self.logger.log(market_name, "POSSIBLE PUMP DETECTED -> LAST PRICE: " + last_fill.Rate);
-        first_ask = last_fill.Rate;
-        // rate = (first_ask * 1.05); // RATE (+) TO BUY ORDER
-        rate = first_ask; // RATE (+) TO BUY ORDER
-        self.pumps_bought[market_name] = true;
-        if (self.test_mode) {
-          self.logger.log(market_name, "Test values: Amount: " + btc_amount * 0.9975 / buy_at * rate + " | Buy price " + buy_at * rate + " | Sell price: " + sell_at * rate);
-        } else {
-          var pump = new PumpHandler(self.pump_events, self.logger, market_name, btc_amount, rate, buy_at, sell_at); // COMMENT THIS LINE FOR REAL TESTING
-          pump.start();
-          self.pumps.push(pump); // later review
-        }
-      } else {
-        self.logger.log(market_name, "PUMP detected on but already started pump handler");
-      }
-      sells = data.Fills.filter(function(e) { return e.OrderType == 'SELL'; });
-      sell_amount = sells.length == 0 ? 0 : sells.map(function(e) { return e.Quantity * e.Rate; }).reduce(function(sum, e) { return sum+e; });
-      winner = buys > sells ? " WINNER: buys" : " WINNER: sells";
-      self.logger.log(market_name, "[CHANGE: " + change + "]" + "[OPEN " + first_fill.TimeStamp + " " + first_fill.Rate + "] [CLOSE " + last_fill.TimeStamp + " " + last_fill.Rate + "] buy amount: " + buys.length + " (" + buy_amount.toFixed(4) + " BTC), sell amount: " + sells.length + "(" + sell_amount.toFixed(4) + " BTC)" + winner);
-    }
+Detektor.prototype.track_tickers_history = function() {
+  if (this.tickers != {}) {
+    Object.keys(this.tickers).forEach((exchange) => {
+      Object.keys(this.tickers[exchange]).forEach((market) => {
+        this.tickers_history[exchange] = this.tickers_history[exchange] || {}
+        this.tickers_history[exchange][market] = this.tickers_history[exchange][market] || []
+        this.tickers_history[exchange][market].push(this.tickers[exchange][market])
+        setTimeout(() => { this.track_tickers_history }, 60 * 1000) // run every minute
+      })
+    })
   }
 }
+
+// Detektor.prototype.analyze_market = function(data) {
+//   result = false;
+//   buy_at = 0;
+//   sell_at = 0;
+//   btc_amount = 0;
+//   market_name = data.MarketName;
+//   self = this;
+//   if (data.Fills.length > 0) {
+//     buys = data.Fills.filter(function(e) { return e.OrderType == 'BUY'; });
+//     buy_amount = buys.length == 0 ? 0 : buys.map(function(e) { return e.Quantity * e.Rate; }).sum();
+//     first_fill = data.Fills.last();
+//     last_fill = data.Fills.first();
+//     change = last_fill.Rate / first_fill.Rate;
+//     if ( (change > 1.04) && (buys.length > 40 || buy_amount > 3.5) ) {
+//       result = true; buy_at = 1.01; sell_at = 1.08; btc_amount = 0.01;
+//     }
+//     if ( (change > 1.05) && (buys.length > 50 || buy_amount > 5) ) {
+//       result = true; buy_at = 1.06; sell_at = 1.5; btc_amount = 0.1;
+//     }
+//   }
+//   if (result) {
+//     self.count += 1;
+//     if (self.count > 100) {
+//       self.logger.log(market_name, "PUMP DETECTED BUT ALREADY REACHED MAX BUY/SELLS")
+//     } else {
+//       if (self.pumps_bought[market_name] == undefined) {
+//         // get ticker info and make BUY order
+//         last_fill = data.Fills.first();
+//         self.logger.log(market_name, "POSSIBLE PUMP DETECTED -> LAST PRICE: " + last_fill.Rate);
+//         first_ask = last_fill.Rate;
+//         // rate = (first_ask * 1.05); // RATE (+) TO BUY ORDER
+//         rate = first_ask; // RATE (+) TO BUY ORDER
+//         self.pumps_bought[market_name] = true;
+//         if (self.test_mode) {
+//           self.logger.log(market_name, "Test values: Amount: " + btc_amount * 0.9975 / buy_at * rate + " | Buy price " + buy_at * rate + " | Sell price: " + sell_at * rate);
+//         } else {
+//           var pump = new PumpHandler(self.pump_events, self.logger, market_name, btc_amount, rate, buy_at, sell_at); // COMMENT THIS LINE FOR REAL TESTING
+//           pump.start();
+//           self.pumps.push(pump); // later review
+//         }
+//       } else {
+//         self.logger.log(market_name, "PUMP detected on but already started pump handler");
+//       }
+//       sells = data.Fills.filter(function(e) { return e.OrderType == 'SELL'; });
+//       sell_amount = sells.length == 0 ? 0 : sells.map(function(e) { return e.Quantity * e.Rate; }).reduce(function(sum, e) { return sum+e; });
+//       winner = buys > sells ? " WINNER: buys" : " WINNER: sells";
+//       self.logger.log(market_name, "[CHANGE: " + change + "]" + "[OPEN " + first_fill.TimeStamp + " " + first_fill.Rate + "] [CLOSE " + last_fill.TimeStamp + " " + last_fill.Rate + "] buy amount: " + buys.length + " (" + buy_amount.toFixed(4) + " BTC), sell amount: " + sells.length + "(" + sell_amount.toFixed(4) + " BTC)" + winner);
+//     }
+//   }
+// }
 
 module.exports = Detektor;
--- run.js
+++ run.js
@@ -5,27 +5,15 @@ require('dotenv').config();
 const Detektor = require('./detektor');
 const EventEmitter = require('events');
 const Logger = require('./logger');
+const Bittrex = require('./bittrex');
 require('./klient');
 
 // Initializers
 class PumpEvents extends EventEmitter {}
 var pump_events = new PumpEvents();
 var test_mode = true;
-var logger = new Logger(true, test_mode);
+var logger = new Logger(false, test_mode);
+var bittrex = new Bittrex(pump_events);
 
-// API
-var api = require('node.bittrex.api');
-api.options({
-  'apikey' : process.env.KEY,
-  'apisecret' : process.env.SECRET,
-  'stream' : false, // will be removed from future versions
-  'verbose' : false,
-  'cleartext' : false 
-});
-
-
-markets = ["BTC-1ST", "BTC-2GIVE", "BTC-ABY", "BTC-ADT", "BTC-AEON", "BTC-AGRS", "BTC-AMP", "BTC-ANS", "BTC-ANT", "BTC-APX", "BTC-ARDR", "BTC-ARK", "BTC-AUR", "BTC-BAT", "BTC-BAY", "BTC-BCY", "BTC-BITB", "BTC-BLITZ", "BTC-BLK", "BTC-BLOCK", "BTC-BNT", "BTC-BRK", "BTC-BRX", "BTC-BSD", "BTC-BTA", "BTC-BTCD", "BTC-BTS", "BTC-BURST", "BTC-BYC", "BTC-CANN", "BTC-CFI", "BTC-CLAM", "BTC-CLOAK", "BTC-CLUB", "BTC-COVAL", "BTC-CPC", "BTC-CRB", "BTC-CRW", "BTC-CURE", "BTC-DAR", "BTC-DASH", "BTC-DCR", "BTC-DCT", "BTC-DGB", "BTC-DGD", "BTC-DMD", "BTC-DOGE", "BTC-DOPE", "BTC-DRACO", "BTC-DTB", "BTC-DYN", "BTC-EBST", "BTC-EDG", "BTC-EFL", "BTC-EGC", "BTC-EMC", "BTC-EMC2", "BTC-ENRG", "BTC-ERC", "BTC-ETC", "BTC-ETH", "BTC-EXCL", "BTC-EXP", "BTC-FAIR", "BTC-FCT", "BTC-FLDC", "BTC-FLO", "BTC-FTC", "BTC-GAM", "BTC-GAME", "BTC-GBG", "BTC-GBYTE", "BTC-GCR", "BTC-GEO", "BTC-GLD", "BTC-GNO", "BTC-GNT", "BTC-GOLOS", "BTC-GRC", "BTC-GRS", "BTC-GUP", "BTC-HKG", "BTC-HMQ", "BTC-INCNT", "BTC-INFX", "BTC-IOC", "BTC-ION", "BTC-IOP", "BTC-KMD", "BTC-KORE", "BTC-LBC", "BTC-LGD", "BTC-LMC", "BTC-LSK", "BTC-LTC", "BTC-LUN", "BTC-MAID", "BTC-MCO", "BTC-MEME", "BTC-MLN", "BTC-MONA", "BTC-MUE", "BTC-MUSIC", "BTC-MYR", "BTC-MYST", "BTC-NAUT", "BTC-NAV", "BTC-NBT", "BTC-NEOS", "BTC-NLG", "BTC-NMR", "BTC-NXC", "BTC-NXS", "BTC-NXT", "BTC-OK", "BTC-OMNI", "BTC-PDC", "BTC-PINK", "BTC-PIVX", "BTC-PKB", "BTC-POT", "BTC-PPC", "BTC-PTC", "BTC-PTOY", "BTC-QRL", "BTC-QWARK", "BTC-RADS", "BTC-RBY", "BTC-RDD", "BTC-REP", "BTC-RISE", "BTC-RLC", "BTC-SBD", "BTC-SC", "BTC-SEC", "BTC-SEQ", "BTC-SHIFT", "BTC-SIB", "BTC-SJCX", "BTC-SLR", "BTC-SLS", "BTC-SNGLS", "BTC-SNRG", "BTC-SNT", "BTC-SPHR", "BTC-SPR", "BTC-START", "BTC-STEEM", "BTC-STRAT", "BTC-SWIFT", "BTC-SWT", "BTC-SYNX", "BTC-SYS", "BTC-THC", "BTC-TIME", "BTC-TKN", "BTC-TKS", "BTC-TRIG", "BTC-TRST", "BTC-TRUST", "BTC-TX", "BTC-UBQ", "BTC-UNB", "BTC-UNO", "BTC-VIA", "BTC-VOX", "BTC-VRC", "BTC-VRM", "BTC-VTC", "BTC-VTR", "BTC-WAVES", "BTC-WINGS", "BTC-XAUR", "BTC-XBB", "BTC-XCP", "BTC-XDN", "BTC-XEL", "BTC-XEM", "BTC-XLM", "BTC-XMG", "BTC-XMR", "BTC-XRP", "BTC-XST", "BTC-XVC", "BTC-XVG", "BTC-XWC", "BTC-XZC", "BTC-ZCL", "BTC-ZEC", "BTC-ZEN", "BTC-FUN"];
-
-detektor = new Detektor(logger, api, markets, pump_events, test_mode);
-
-detektor.start();
+// Start
+detektor = new Detektor(logger, pump_events, test_mode)
