6501172a154012c93c59cfaef6c6c2f37d87d87b
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-07-11 23:47:11 -0500

    Standarize calls to match api standards

--- yobit.js
+++ yobit.js
@@ -7,20 +7,17 @@ function YobitClient(pump_events) {
   this.markets = [];
   this.market_data = [];
   this.pump_events = pump_events;
+}
 
-  this.client.publicRequest('info', {}, (err, e) => {
+YobitClient.prototype.watch = function() {
+  this.get_markets((err, e) => {
     Object.keys(e.pairs).forEach((key) => {
       if (e.pairs[key].hidden == 0 && key.endsWith('btc'))
         this.markets.push(key)
     })
   });
 
-
   setTimeout(() => { this._watch_tickers() }, 10 * 1000)
-}
-
-YobitClient.prototype.getmarkets = function(callback) {
-  this.client.getmarketsummaries( callback );
 }
 
 YobitClient.prototype._watch_tickers = function() {
@@ -41,6 +38,85 @@ YobitClient.prototype._watch_tickers = function() {
   setTimeout(() => { this._watch_tickers() }, 10 * 1000)
 }
 
+// Implement standard functions
+YobitClient.prototype.balance = function(callback) {
+  this.client.getInfo(callback) // implement later
+}
+
+YobitClient.prototype.get_markets = function(callback) {
+  this.client.publicRequest('info', {}, callback)
+}
+
+YobitClient.prototype.get_order = function(order_id, callback) {
+  this.client.privateRequest('OrderInfo', { order_id: order_id }, (err, data) => {
+    callback({
+      success: data.success == 1,
+      message: data.error,
+      result: this._parse_order(data.return)
+    })
+  })
+}
+
+YobitClient.prototype.get_orders = function(market, callback) {
+  market = market.toLowerCase().replace(/\-/, '_')
+  this.client.privateRequest('ActiveOrders', { pair: market }, (err, data) => {
+    callback({
+      success: data.success == 1,
+      message: data.error,
+      result: this._parse_order(data.return)
+    })
+  })
+}
+
+YobitClient.prototype.get_all_orders = function(callback) { // pair is required
+  this.client.privateRequest('ActiveOrders', { }, (err, data) => {
+    callback({
+      success: data.success == 1,
+      message: data.error,
+      result: this._parse_order(data.return)
+    })
+  })
+}
+
+YobitClient.prototype.buy_order = function(market, quantity, rate, callback) {
+  market = market.toLowerCase().replace(/\-/, '_')
+  params = { pair: market, type: 'buy', rate: rate, amount: quantity }
+  this.client.privateRequest('Trade', params, (err, data) => {
+    callback(
+      { success: data.success == 1,
+      message: data.error,
+      result: { id: data.return.order_id } }
+    )
+  })
+}
+
+YobitClient.prototype.sell_order = function(market, quantity, rate, callback) {
+  market = market.toLowerCase().replace(/\-/, '_')
+  params = { pair: market, type: 'sell', rate: rate, amount: quantity }
+  this.client.privateRequest('Trade', params, (err, data) => {
+    callback(
+      { success: data.success == 1,
+      message: data.error,
+      result: { id: data.return.order_id } }
+    )
+  })
+}
+
+YobitClient.prototype.cancel_order = function(order_id, callback) {
+  this.client.privateRequest('CancelOrder', { order_id: order_id }, callback)
+}
+
+YobitClient.prototype.cancel_all_orders = function(market) { // emergency function
+  this.get_orders(market, (response) => {
+    response.result.forEach((order, n) => {
+      setTimeout(() => {
+        console.log("canceling order " + order.id)
+        this.cancel_order(order.id, (err, info) => { console.log("order deleted", err, info) })
+      }, 1000 * (n + 1))
+    })
+  })
+}
+
 YobitClient.prototype._normalize_ticker_data = function(data) {
   return {
     high: data.high,
@@ -54,28 +130,22 @@ YobitClient.prototype._normalize_ticker_data = function(data) {
   }
 }
 
-// Implement standard functions
-YobitClient.prototype.balance = function(currency, callback) {
-  this.client.getbalance({ 'currency': currency }, callback);
-}
-
-YobitClient.prototype.get_order = function(order_id, callback) {
-  this.client.getorder({ uuid : orderId }, callback);
-}
-
-YobitClient.prototype.buy_order = function(market, quantity, rate, callback) {
-  var url = 'https://bittrex.com/api/v1.1/market/buylimit?market=' + market + "&quantity=" + quantity + "&rate=" + rate;
-  this.client.sendCustomRequest( url, callback, true );
-}
-
-YobitClient.prototype.sell_order = function(market, quantity, rate, callback) {
-  var url = 'https://bittrex.com/api/v1.1/market/selllimit?market=' + market + "&quantity=" + quantity + "&rate=" + rate;
-  this.client.sendCustomRequest( url, callback, true );
-}
-
-YobitClient.prototype.cancel_order = function(uuid, callback) {
-  var url = 'https://bittrex.com/api/v1.1/market/cancel?uuid=' + uuid;
-  this.client.sendCustomRequest( url, callback, true );
+YobitClient.prototype._parse_order = function(e) {
+  if (e == undefined) return undefined
+  return Object.keys(e).map((order_id) => {
+    order = e[order_id]
+    return {
+      id: order_id,
+      market: order.pair.toUpperCase().replace(/\_/, '-'),
+      type: order.type == 'buy' ? 'ask' : 'bid',
+      quantity: order.start_amount,
+      quantity_remaining: order.amount,
+      price: order.rate,
+      price_per_unit: order.rate,
+      opened: order.timestamp_created,
+      status: order.status
+    }
+  })
 }
 
 module.exports = YobitClient;
