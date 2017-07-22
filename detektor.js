695e4fe2fc82ca60c6b11ee0724c3dd5714acb0e
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-07-22 16:01:04 -0500

    Cleanup

--- detektor.js
+++ detektor.js
@@ -17,14 +17,13 @@ function Detektor(logger, pump_events, test_mode, database, api_clients) {
   this.trade_autotrader_enabled = false // based on TRADE info
   this.ticker_autotrader_enabled = false // based on TICKER info
   this.pumps = []
-  this.max_tickers_history = 60 // minutes
+  this.max_tickers_history = 60 // minutes of history to be kept
+  this.tickers_history_cleaning_time = 20 // clean ever X minutes
   this.database = database
   this.signal = new Signal()
 
-  this.tickers_history_cleaning_time = 20 // minutes
-
   this.exchange_volume_change = {
-    'BTRX': 1.30, // 1.25
+    'BTRX': 1.30,
     'YOBT': 1.3,
     'POLO': 1.25,
     'CPIA': 1.25
@@ -45,12 +44,6 @@ function Detektor(logger, pump_events, test_mode, database, api_clients) {
         this.update_ticker(exchange, market, data)
         this.update_ticker_history(exchange, market, data)
         this.analyze_ticker(exchange, market, data)
-      }
-      if (operation == 'TRADE' && exchange == 'BTRX') {
-        this.trades[exchange] = this.trades[exchange] || {}
-        this.trades[exchange][market] = data;
-
-        this.analyze_market(data)
       }
     }
   })
@@ -218,57 +211,6 @@ Detektor.prototype.market_url = function(exchange, market) {
   }
   if (exchange == 'CPIA') {
     return "https://www.cryptopia.co.nz/Exchange/?market=" + market.replace(/\-/, "_")
-  }
-}
-
-Detektor.prototype.analyze_market = function(data) {
-  if (!this.trade_autotrader_enabled) return '';
-  result = false;
-  buy_at = 0;
-  sell_at = 0;
-  btc_amount = 0;
-  market_name = data.MarketName;
-  self = this;
-  if (data.Fills.length > 0) {
-    buys = data.Fills.filter(function(e) { return e.OrderType == 'BUY'; });
-    buy_amount = buys.length == 0 ? 0 : buys.map(function(e) { return e.Quantity * e.Rate; }).sum();
-    first_fill = data.Fills.last();
-    last_fill = data.Fills.first();
-    change = last_fill.Rate / first_fill.Rate;
-    // if ( (change > 1.04) && (buys.length > 40 || buy_amount > 3.5) ) {
-    //   result = true; buy_at = 1.01; sell_at = 1.08; btc_amount = 0.01;
-    // }
-    if ( (change > 1.05) && (buys.length > 50 || buy_amount > 5) ) {
-      result = true; buy_at = 1.06; sell_at = 1.4; btc_amount = 0.1;
-    }
-  }
-  if (result) {
-    self.count += 1;
-    if (self.count > 100) {
-      self.logger.log("BTRX/" + market_name, "PUMP DETECTED BUT ALREADY REACHED MAX BUY/SELLS", true)
-    } else {
-      if (self.pumps_bought[market_name] == undefined) {
-        // get ticker info and make BUY order
-        last_fill = data.Fills.first();
-        self.logger.log("BTRX/" + market_name, "POSSIBLE PUMP DETECTED -> LAST PRICE: " + last_fill.Rate, true);
-        first_ask = last_fill.Rate;
-        rate = first_ask; // RATE (+) TO BUY ORDER
-        self.pumps_bought[market_name] = true;
-        if (self.test_mode) {
-          self.logger.log("BTRX/" + market_name, "Test values: Amount: " + btc_amount * 0.9975 / buy_at * rate + " | Buy price " + buy_at * rate + " | Sell price: " + sell_at * rate, true);
-        } else {
-          var pump = new PumpHandler(self.pump_events, self.logger, self.api_clients[exchange], 'BTRX', market_name, btc_amount, rate, buy_at, sell_at, self, 1, this.verbose); // COMMENT THIS LINE FOR REAL TESTING
-          pump.start();
-          self.pumps.push(pump); // later review
-        }
-      } else {
-        self.logger.log("BTRX/" + market_name, "PUMP detected on but already started pump handler", true);
-      }
-      sells = data.Fills.filter(function(e) { return e.OrderType == 'SELL'; });
-      sell_amount = sells.length == 0 ? 0 : sells.map(function(e) { return e.Quantity * e.Rate; }).reduce(function(sum, e) { return sum+e; });
-      winner = buys > sells ? " WINNER: buys" : " WINNER: sells";
-      self.logger.log("BTRX/" + market_name, "[CHANGE: " + change + "]" + "[OPEN " + first_fill.TimeStamp + " " + first_fill.Rate + "] [CLOSE " + last_fill.TimeStamp + " " + last_fill.Rate + "] buy amount: " + buys.length + " (" + buy_amount.toFixed(4) + " BTC), sell amount: " + sells.length + "(" + sell_amount.toFixed(4) + " BTC)" + winner, true);
-    }
   }
 }
 
