43a69565ff8ea68d625fc9d69ee1aec3086d6e03
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-08-01 00:08:47 -0500

    Add wallet class for handling blockchain account

---
+++ wallet.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const MyWallet = require('blockchain.info/MyWallet')
+
+// For each wallet, we can have 20 simultaneous payments
+// If we get past those 20 payments, we need to generate another wallet for the new payment address
+// When a payment is completed, "close" the address (remove from recognized addresses)
+// Keep  track of: payments, wallets and generated payment addresses
+// payment: subscriber_id, wallet (main), payment_address, amount, date
+// wallet: address, xpub
+
+var options = { apiCode: process.env.BLOCKCHAIN_API_CODE, apiHost: 'http://localhost:3000' }
+
+function Wallet() {
+  this.wallet =  new MyWallet(process.env.WALLET_UID, process.env.WALLET_PASSWD, options)
+  this.accounts = []
+  this.refresh()
+}
+
+Wallet.prototype.refresh = function() {
+  this._accounts().then((data) => { this.accounts = data; })
+}
+
+Wallet.prototype.create_account = function(label = 'main') {
+  this.wallet.createAccount({label: label}).then(
+    (data) => { this.refresh(); return this.accounts[this.accounts.length]; }
+  )
+}
+
+Wallet.prototype._accounts = function() { return this.wallet.listAccounts() }
+
+module.exports = Wallet;
+
+// accounts
+// extendedPublicKey: 'XPUB FOR CREATING PAYMENT ADDRESSES'
+
+// create account
+// { archived: false,
+//   xpriv: '...',
+//   xpub: 'XPUB FOR CREATING PAYMENT ADDRESSES',
+//   address_labels: [],
+//   cache: 
+//    { receiveAccount: '...',
+//      changeAccount: '...' } }
