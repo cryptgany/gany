964e0d5e3053dee0a687ca9ef0739feed3869742
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2017-08-06 11:46:23 -0500

    Working price rates based on subscription types

--- wallet.js
+++ wallet.js
@@ -6,7 +6,11 @@ function Wallet(gany_the_bot) {
   this.subscriber_list = [] // address list to check
   this.days_for_subscription_ending = 2 // days
   this.gany_the_bot = gany_the_bot
-  this.subscription_price = 300000 // now 0.003, should be 1000000
+  this.subscription_price = {
+    basic: 300000, // now 0.003, should be 1000000
+    advanced: 3000000, // 0.03
+    pro: 5000000, // 0.05
+  }
 }
 
 Wallet.prototype.track_subscriptions = function() {
@@ -27,25 +31,27 @@ Wallet.prototype.refresh = function() { // update list of accounts to check
 Wallet.prototype.check_transactions = function() {
   // should check every subscriber's address for balance
   // if person has balance >= 0.01 then sechedule address for withdrawal and mark as subscribed
-  blockexplorer.getBalance(this.subscriber_list.map((sub) => { return sub.btc_address }), this.options).then((addr_data) => {
+  addresses_sub_type = {}
+  this.subscriber_list.forEach((sub) => { addresses_sub_type[sub.btc_address] = sub.subscription_type })
+  blockexplorer.getBalance(Object.keys(addresses_sub_type), this.options).then((addr_data) => {
     Object.keys(addr_data).forEach((addr) => {
-      if (addr_data[addr].final_balance >= this.subscription_price) { // now 0.003, should be 1000000
+      if (addr_data[addr].final_balance >= this.subscription_price[addresses_sub_type[addr]]) { // now 0.003, should be 1000000
         // detected address with enough money, mark subscriber as subscribed and schedule for withdrawal
-        this.mark_subscriber_paid_and_withdraw(addr)
+        this.mark_subscriber_paid_and_withdraw(addr, this.subscription_price[addresses_sub_type[addr]])
       }
     })
   }).catch((e) => { console.log("Error on check_transactions", Date.now(), e) })
   setTimeout(() => { this.check_transactions() }, 5 * 60 * 1000)
 }
 
-Wallet.prototype.mark_subscriber_paid_and_withdraw = function(address) {
+Wallet.prototype.mark_subscriber_paid_and_withdraw = function(address, price) {
   subscriber = this.subscriber_list.filter((sub) => { return sub.btc_address == address })[0]
   if (subscriber) {
     // alert subscriber that we got his payment and until when he/she is subcribed
     subscriber.set_subscription_confirmed()
     this.gany_the_bot.notify_user_got_confirmed(subscriber)
 
-    this.schedule_for_withdrawal(subscriber.telegram_id, address, subscriber.btc_private_key, this.subscription_price)
+    this.schedule_for_withdrawal(subscriber.telegram_id, address, subscriber.btc_private_key, price)
   }
 }
 
