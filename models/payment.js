0891923a04383fbddec93fff2c700ca90ff6a8eb
Carlos Rodriguez <carlosr706@gmail.com>
2017-12-24 13:27:40 -0500

    Increase amount for transactions so we actually receive the money

--- models/payment.js
+++ models/payment.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/detektor'
 
 const PROCESS_MAXIMUM_INPUTS = 60 // maximum addresses into one transaction
 const CHECK_PAYMENTS_EVERY = 1 // hours
-const SATS_FEE_PER_BYTE = 25 // in satoshis
+const SATS_FEE_PER_BYTE = 250 // in satoshis
 const MIN_PAYMENTS_TO_PROCESS = 5 // minimum pending payments to start a transaction
 const MIN_CONFIRMATIONS_TO_PROCESS = 3 // tx confirmations
 
