db90ebfe15fcb3be055916cff8b67b8dfca8f2d9
Carlos Rodriguez <carlosr706@gmail.com>
2019-04-06 12:08:36 -0300

    #118 add alert spec for trigger

--- test/index.js
+++ test/index.js
@@ -1,2 +1,6 @@
+// COMMON SHARED LIBS TESTS
 require("./commons/protofunctions_test")
 require("./commons/user_input_analyzer_test")
+
+// MODELS TESTS
+require("./models/alert_test")
---
+++ test/models/alert_test.js
@@ -0,0 +1,25 @@
+const Alert = require('../../models/alert')
+var assert = require('assert');
+
+describe('Alert functions', function() {
+	it('trigger(price) returns true when price is going up and alert is up', function(done) {
+		let alert = new Alert({price_start: 100, price_target: 120})
+		assert.equal(alert.trigger(130), true)
+		assert.equal(alert.trigger(120.000001), true)
+		assert.equal(alert.trigger(120), true)
+		assert.equal(alert.trigger(119.9999), false)
+		assert.equal(alert.trigger(90), false)
+		done();
+	});
+
+	it('trigger(price) returns true when price is going down and alert is down', function(done) {
+		let alert = new Alert({price_start: 100, price_target: 80})
+		assert.equal(alert.trigger(79), true)
+		assert.equal(alert.trigger(79.9999), true)
+		assert.equal(alert.trigger(80), true)
+		assert.equal(alert.trigger(80.01), false)
+		assert.equal(alert.trigger(120), false)
+		assert.equal(alert.trigger(85), false)
+		done();
+	});
+});
