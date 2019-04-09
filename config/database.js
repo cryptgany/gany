46a65f1649baca168df9b10f2cdd45cb1f918fc6
Carlos Rodriguez <carlosr706@gmail.com>
2019-04-08 23:05:41 -0400

    #118 fix

--- config/database.js
+++ config/database.js
@@ -3,5 +3,6 @@ if (process.env.ENVIRONMENT == 'development') {
 	mongoose.connect('mongodb://localhost:27017/detektor');
 } else if (process.env.ENVIRONMENT == 'test') {
 	mongoose.connect('mongodb://localhost:27017/detektor-test');
+} else {
+	mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/detektor');
 }
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/detektor');
